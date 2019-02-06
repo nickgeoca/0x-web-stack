89a73f8377e1c7c0ed4c1569f3728cbca5188f69
Nick Geoca <nickgeoca@gmail.com>
2019-02-06 14:55:45 -0800

    First metamask tx - wrap eth

--- src/pages/Dashboard/EthToWeth.vue
+++ src/pages/Dashboard/EthToWeth.vue
@@ -4,49 +4,48 @@
   </div>
 </template>
 <script>
-import { ContractWrappers } from '0x.js';
+import { BigNumber, ContractWrappers } from '0x.js';
+import { Web3Wrapper } from '@0x/web3-wrapper';
 import { State } from '../../main'
 
 export default {
   methods: {
-    ethToWeth: function (event) {
-      const etherTokenAddress = State.contractWrappers.forwarder.etherTokenAddress;
-      console.log(etherTokenAddress);
-      
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this.name + '!')
-      // `event` is the native DOM event
-      if (event) {
-        alert(event.target.tagName)
-      }
+    ethToWeth: async (event) => {
+      await wrapOrUnwrapEthAsync(true);
     }
   }
 }
-/*
-public wrapOrUnwrapEthAsync = async (wrap: boolean) => {
-  const { web3Wrapper, contractWrappers, onTxSubmitted } = this.props;
-  const amount 10;
 
-  // Retrieve the ether token address
+const onTxSubmitted = txHash => { console.log(txHash); };
+
+var wrapOrUnwrapEthAsync = async (wrap) => {
+  const web3Wrapper = State.web3Wrapper;
+  const contractWrappers = State.contractWrappers;
+
+  const amount = 0.5;
   const etherTokenAddress = contractWrappers.forwarder.etherTokenAddress;
+
   if (etherTokenAddress) {
     // List all of the available addresses
     const addresses = await web3Wrapper.getAvailableAddressesAsync();
+
     // The first address is one the which deposits or withdraws Eth
     const account = addresses[0];
+
     // Amounts are in ETH, this needs to be converted into their base unit wei
     const ethAmount = new BigNumber(amount);
     const weiAmount = Web3Wrapper.toBaseUnitAmount(ethAmount, 18);
+
     // Call deposit or withdraw on the ethertoken
     const txHash = wrap
           ? await contractWrappers.etherToken.depositAsync(etherTokenAddress, weiAmount, account)
           : await contractWrappers.etherToken.withdrawAsync(etherTokenAddress, weiAmount, account);
+
     if (txHash) {
       onTxSubmitted(txHash);
     }
   }
-}*/
-
+}
 
 </script>
 <style>
