c7732b6a60ca74cc0152ac04d6e355e62e0506c4
Nick Geoca <nickgeoca@gmail.com>
2019-02-06 14:35:38 -0800

    Print eth address in console

--- src/pages/Dashboard/EthToWeth.vue
+++ src/pages/Dashboard/EthToWeth.vue
@@ -4,48 +4,50 @@
   </div>
 </template>
 <script>
-// import { ContractWrappers } from '0x.js';
+import { ContractWrappers } from '0x.js';
+import { State } from '../../main'
 
-  export default {
-    methods: {
-      ethToWeth: function (event) {
-        // const etherTokenAddress = contractWrappers.forwarder.etherTokenAddress;
-        // console.log(etherTokenAddress);
-        
-        // `this` inside methods points to the Vue instance
-        alert('Hello ' + this.name + '!')
-        // `event` is the native DOM event
-        if (event) {
-          alert(event.target.tagName)
-        }
+export default {
+  methods: {
+    ethToWeth: function (event) {
+      const etherTokenAddress = State.contractWrappers.forwarder.etherTokenAddress;
+      console.log(etherTokenAddress);
+      
+      // `this` inside methods points to the Vue instance
+      alert('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
       }
     }
   }
+}
+/*
+public wrapOrUnwrapEthAsync = async (wrap: boolean) => {
+  const { web3Wrapper, contractWrappers, onTxSubmitted } = this.props;
+  const amount 10;
+
+  // Retrieve the ether token address
+  const etherTokenAddress = contractWrappers.forwarder.etherTokenAddress;
+  if (etherTokenAddress) {
+    // List all of the available addresses
+    const addresses = await web3Wrapper.getAvailableAddressesAsync();
+    // The first address is one the which deposits or withdraws Eth
+    const account = addresses[0];
+    // Amounts are in ETH, this needs to be converted into their base unit wei
+    const ethAmount = new BigNumber(amount);
+    const weiAmount = Web3Wrapper.toBaseUnitAmount(ethAmount, 18);
+    // Call deposit or withdraw on the ethertoken
+    const txHash = wrap
+          ? await contractWrappers.etherToken.depositAsync(etherTokenAddress, weiAmount, account)
+          : await contractWrappers.etherToken.withdrawAsync(etherTokenAddress, weiAmount, account);
+    if (txHash) {
+      onTxSubmitted(txHash);
+    }
+  }
+}*/
+
+
 </script>
 <style>
 </style>
-
-<!--
-    public wrapOrUnwrapEthAsync = async (wrap: boolean) => {
-        const { web3Wrapper, contractWrappers, onTxSubmitted } = this.props;
-        const { amount } = this.state;
-        // Retrieve the ether token address
-        const etherTokenAddress = contractWrappers.forwarder.etherTokenAddress;
-        if (etherTokenAddress) {
-            // List all of the available addresses
-            const addresses = await web3Wrapper.getAvailableAddressesAsync();
-            // The first address is one the which deposits or withdraws Eth
-            const account = addresses[0];
-            // Amounts are in ETH, this needs to be converted into their base unit wei
-            const ethAmount = new BigNumber(amount);
-            const weiAmount = Web3Wrapper.toBaseUnitAmount(ethAmount, 18);
-            // Call deposit or withdraw on the ethertoken
-            const txHash = wrap
-                ? await contractWrappers.etherToken.depositAsync(etherTokenAddress, weiAmount, account)
-                : await contractWrappers.etherToken.withdrawAsync(etherTokenAddress, weiAmount, account);
-            if (txHash) {
-                onTxSubmitted(txHash);
-            }
-        }
-    }
--->
