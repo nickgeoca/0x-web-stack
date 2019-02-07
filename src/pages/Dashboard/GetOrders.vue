54ae223c1f2d6d881f3481523a4d9e39ef50f73e
Nick Geoca <nickgeoca@gmail.com>
2019-02-07 12:52:38 -0800

    Gets orders from relayer

--- src/pages/Dashboard/GetOrders.vue
+++ src/pages/Dashboard/GetOrders.vue
@@ -2,10 +2,20 @@
 
 <div id="getOrders">
   <form>
-    <base-input v-model="amount"
-                type="text"
-                placeholder="Amount">
-    </base-input>
+    <div class="form-row">
+      <base-input v-model="makerSym"
+                  label="Buy Symbol"
+                  class="col-md-2"
+                  type="text"
+                  placeholder="symbol">
+      </base-input>
+      <base-input v-model="takerSym"
+                  label="Sell Symbol"
+                  class="col-md-6"
+                  type="text"
+                  placeholder="symbol">
+      </base-input>
+    </div>
     <base-button type="default" v-on:click="getOrders">Get Orders</base-button>
   </form>
 </div>
@@ -13,7 +23,7 @@
 </template>
 
 <script>
-import { BigNumber, ContractWrappers } from '0x.js';
+import { BigNumber, ContractWrappers , assetDataUtils } from '0x.js';
 import { Web3Wrapper } from '@0x/web3-wrapper';
 import { State } from './../../main'
 import { ETHER_TOKEN, TOKENS_BY_NETWORK } from '../../tokens';
@@ -22,58 +32,35 @@ import { HttpClient } from '@0x/connect';
 
 export default {
   data() {
-    return { amount: '' }
+    return { makerSym: '', takerSym: '' }
   },
   methods: {
     getOrders: async function (event) {
-      // assetDataUtils
-      // makerToken
-      const web3Wrapper = State.web3Wrapper;
-      const networkId = await web3Wrapper.getNetworkIdAsync();
-      const tokensForNetwork = TOKENS_BY_NETWORK[networkId];
-      const makerToken = tokensForNetwork['ZRX'];
-      assetDataUtils.encodeERC20AssetData(makerToken.address);
-      const orderbookRequest = { baseAssetData: makerAssetData, quoteAssetData: takerAssetData };
-
-      const httpClient = new HttpClient('http://localhost:3000/v2/');
-      const response = await httpClient.getOrderbookAsync(orderbookRequest, { networkId: NETWORK_CONFIGS.networkId });
-      if (response.asks.total === 0) {
-        throw new Error('No orders found on the SRA Endpoint');
-      }
-      console.log(response);
-      return response;
+      await getOrdersAsync(this.makerSym, this.takerSym);
     }
   }
 }
 
 const onTxSubmitted = txHash => { console.log(txHash); };
 
-var wrapOrUnwrapEthAsync = async (wrap, amount) => {
+var getOrdersAsync = async (makerSym, takerSym) => {
   const web3Wrapper = State.web3Wrapper;
-  const contractWrappers = State.contractWrappers;
+  const networkId = await web3Wrapper.getNetworkIdAsync();
+  const tokensForNetwork = TOKENS_BY_NETWORK[networkId];
+  const makerToken = tokensForNetwork[makerSym];
+  const takerToken = tokensForNetwork[takerSym];
 
-  const etherTokenAddress = contractWrappers.forwarder.etherTokenAddress;
+  const makerAssetData = assetDataUtils.encodeERC20AssetData(makerToken.address);
+  const takerAssetData = assetDataUtils.encodeERC20AssetData(takerToken.address);
+  const orderbookRequest = { baseAssetData: makerAssetData, quoteAssetData: takerAssetData };
 
-  if (etherTokenAddress) {
-    // List all of the available addresses
-    const addresses = await web3Wrapper.getAvailableAddressesAsync();
-
-    // The first address is one the which deposits or withdraws Eth
-    const account = addresses[0];
-
-    // Amounts are in ETH, this needs to be converted into their base unit wei
-    const ethAmount = new BigNumber(amount);
-    const weiAmount = Web3Wrapper.toBaseUnitAmount(ethAmount, 18);
-
-    // Call deposit or withdraw on the ethertoken
-    const txHash = wrap
-          ? await contractWrappers.etherToken.depositAsync(etherTokenAddress, weiAmount, account)
-          : await contractWrappers.etherToken.withdrawAsync(etherTokenAddress, weiAmount, account);
-
-    if (txHash) {
-      onTxSubmitted(txHash);
-    }
+  const httpClient = new HttpClient('http://localhost:3000/v2/');
+  const response = await httpClient.getOrderbookAsync(orderbookRequest, { networkId: NETWORK_CONFIGS.networkId });
+  if (response.asks.total === 0) {
+    throw new Error('No orders found on the SRA Endpoint');
   }
+  console.log(response);
+  return response;
 }
 
 </script>
