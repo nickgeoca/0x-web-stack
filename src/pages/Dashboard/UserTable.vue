5714aa0ebe5da789028b91beb807c3d28d39cc9b
Nick Geoca <nickgeoca@gmail.com>
2019-02-06 17:47:08 -0800

    Reverting usertable to original code

--- src/pages/Dashboard/UserTable.vue
+++ src/pages/Dashboard/UserTable.vue
@@ -5,33 +5,14 @@
   </base-table>
 </template>
 <script>
-import { BaseTable } from "@/components";
-export default {
-  components: {
-    BaseTable
-  },
-  computed: {
-
-    table(){
-    const d = { columns: ["Name", "Symbol", "Balance", "Allowance"]
-              , data: []
-              };
-
-    d.data.push({"id": 1, "name": , "symbol": , "balance": , "allowance"});
-
-      // name, decimals, symbol, isTradeable, isMintable, image, address, balance, allowance
-      return 
-      {"0x22d5ebe7cd2554b9d02579cfe61436f81ae90b09"
- :[
-{"token" : {"name":"0x Protocol Token"
-           ,"decimals":18
-           ,"symbol":"ZRX"
-           ,"isTradeable":true
-           ,"isMintable":false
-           ,"image":"https://0xproject.com/images/token_icons/ZRX.png"
-           ,"address":"0x871dd7c2b4b25e1aa18728e9d5f2af4c4e431f5c"
-,"balance":"0"
-,"allowance":"115792089237316195423570985008687907853269984665640564039457584007913129639935"
+  import { BaseTable } from "@/components";
+  export default {
+    components: {
+      BaseTable
+    },
+    computed: {
+      table(){
+        return this.$t('dashboard.usersTable');
       }
     }
   }
